965e813cf3689a9ea0b230505e9f1811baec6133
DUS2002 <daniurdi46@gmail.com>
2020-07-30 18:14:36 +0000

    Initial add

---
+++ usershares_setup.sh
@@ -0,0 +1,16 @@
+﻿#!/bin/bash
+
+unset $REPLY
+read -p "Type the name of your normal user: "
+ 
+# Set variables
+USERSHARES_DIR="/var/lib/samba/usershares"
+USERSHARES_GROUP="sambashare"
+
+# Setup
+mkdir -p ${USERSHARES_DIR}
+groupadd ${USERSHARES_GROUP}
+chown root:${USERSHARES_GROUP} ${USERSHARES_DIR}
+chmod 01770 ${USERSHARES_DIR}
+usermod -a -G ${USERSHARES_GROUP} ${REPLY}
+
